12dc45c6acfd2edaaa515bc3287e21435b152094
Corey <coreymemerson@gmail.com>
2017-09-27 21:27:34 -0400

    initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 //Emerson Branch
-
 public class DemoApplication {
 
 	public static void main(String[] args) {
